4cea06816419d26cdca605db4cab10568ac82626
lim2002 <113607876+lim2002@users.noreply.github.com>
2022-10-06 11:36:28 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# clase-vue
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-tebsrv)
---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h1>Hola Mundo</h1>
+    <img
+      alt="Vue logo"
+      src="https://img.wattpad.com/cover/258103381-352-k807196.jpg"
+    />
+    <HelloWorld msg="Hola Mundo" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
